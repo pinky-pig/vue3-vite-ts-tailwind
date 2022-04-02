d475c521a46a289f8669da3d763340f92034f5b4
pinky-pig <2545589993@qq.com>
2022-04-02 15:43:23 +0800

    使用windicss

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,7 +1,7 @@
-import './default.css';
-/* import './naiveUI.css'; */
-import './tailwind.css';
-import './theme.less';
+import './default.css'
+import './theme.less'
+// 引入windiCSS
+import 'virtual:windi.css'
 
 /** 引入静态资源(全局引入css、字体等) */
 function setupStyle() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import WindiCSS from 'vite-plugin-windicss'
 
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(),WindiCSS()],
   css: {
     preprocessorOptions: {
       less: {
