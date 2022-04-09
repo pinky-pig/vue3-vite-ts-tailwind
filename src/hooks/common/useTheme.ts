ef3a7276a4168fd1b06b2b385537fe00c620f44b
pinky-pig <2545589993@qq.com>
2022-04-09 16:49:38 +0800

    使用useDark

--- src/hooks/common/useTheme.ts
+++ src/hooks/common/useTheme.ts
@@ -1,7 +1,14 @@
 /**
- * 改变全局定义的主题变量
+ * 改变全局定义的主题变量。使用了vueUse的useDark，此方法废弃。如果改变多套颜色变量可使用。
  * @param key 定义的变量key
- * @returns
+ * @example
+ * const changeTheme = useTheme();
+ * const changeThemeFunc = () => {
+ *   isDark.value ? changeTheme('dark') : changeTheme('default')
+ * }
+ * const changeThemeFunc = () => {
+ *   isDark.value ? changeTheme('dark') : changeTheme('default')
+ * }
  */
 export default function useTheme(key = 'theme') {
   return (theme: string) => {
--- src/layout/navigation_bar/Index.vue
+++ src/layout/navigation_bar/Index.vue
@@ -6,27 +6,15 @@
     <div class=" flex flex-1 flex-row justify-between">
       <div></div>
       <div class=" flex-y-center">
-        <!-- <Icon :icon="isDark ? 'akar-icons:moon' : 'akar-icons:sun'"
-          class=" w-6 h-6 cursor-pointer"
-          @click="changeThemeFunc();toggleDark()"
-        /> -->
-
+        <Icon :icon="isDark ? 'akar-icons:moon' : 'akar-icons:sun'"
+          class=" w-6 h-6 cursor-pointer" @click="toggleDark()" />
       </div>
     </div>
   </div>
 </template>
 <script setup lang='ts'>
-import { useTheme } from "@/hooks"
 import { Icon } from '@iconify/vue';
-// import { isDark, toggleDark } from '@/plugins'
-
-const changeTheme = useTheme();
-// const themeSwitch = ref(true)
-/**切换预置主题颜色 */
-const changeThemeFunc = () => {
-  isDark.value ? changeTheme('dark') : changeTheme('default')
-}
-
+import { isDark, toggleDark } from '@/plugins'
 </script>
 
 <style lang='less' module>
