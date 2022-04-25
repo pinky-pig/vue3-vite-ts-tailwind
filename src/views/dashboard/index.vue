24b932c40724f16abf3a9449147b225a65293979
pinky-pig <2545589993@qq.com>
2022-04-25 09:21:56 +0800

    .

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class='$style["dashboard"]'>
+    <h3>1.项目嵌套</h3>
+    <h3>2.组件渲染前的loading</h3>
+    <h3>3.https://picsum.photos/40</h3>
+    <h3>4.vue-plugin-pages 基于文件系统的路由生成器</h3>
+    <h3>5.Teleport</h3>
+    <h3>6.v-bind:title="message"</h3>
+    <h3>7.大文件上传</h3>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style lang='less' module>
+.dashboard{
+  @apply wh-full flex-col;
+  background: var(--layout-main-bg);
+  color: var(--layout-main-text);
+  border-radius: 2%;
+  padding: 0 2rem;
+}
+</style>
